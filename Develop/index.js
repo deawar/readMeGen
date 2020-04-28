db94e93a5d19b1923c1d820262638d06cabde4b4
dean <dean@SSCCBISHOP.LOCAL>
2020-04-28 19:17:20 -0400

    Refactoring directory structure

--- Develop/index.js
+++ Develop/index.js
@@ -5,13 +5,16 @@
 -Populate Readme.medium
 
 */
-//const axios = require('axios');
+const axios = require('axios');
 //const dotenv = require('dotenv');
 //const electron = require('electron');
 //const electronHtmlTo = require('electron-html-to');
 const inquirer = require('inquirer');
 const fs = require('fs');
 //const open = require('open');
+console.log(process.env.SECRET_MESSAGE);
+
+
 const questions = [
   {
     type: "input",
@@ -35,33 +38,69 @@ inquirer
   .then(answers => {
     // Use user feedback for... whatever!!
     console.log("Hello world!")
+    
+    // console.log(answers)
+    // console.log(answers.projectName)
+    // const {username, email, projectName} = answers
+    // console.log(username, email, projectName)
+    writeToFile(answers, "ReadME.md")
   })
   .catch(error => {
     if (error.isTtyError) {
+      console.log("Inquierer: unable to save to file: ",error)
       // Prompt couldn't be rendered in the current environment
     } else {
       // Something else when wrong
     }
   });
 
-function writeToFile (data, filename) {
-  const header = `# ${projectTitle} 
+  const getUsername = async (username) => {
 
-    ![https://img.shields.io/badge/Node.js-v12.16.2-brightgreen] ![https://img.shields.io/badge/Electron-v.6.14.4-blue]
-    ${projectDescription}
-    <html lang="en">
-    <head>
-        <meta charset="UTF-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <title>Mini Project 40</title>
-    </head>
-    <body>
-        <div class="container">`;
-  fs.writeFile(filename + ".html", header + data, function (err) {
-    if (err) {
-      return console.log(err);
-    }
-  });
+    try{
+      console.log(username);
+      const gitUrl = `https://api.github.com/users/${username}`
+      await axios.get(gitUrl)
+          .then(response =>{
+            const {login, avatar_url, repos_url, email} = response.data;
+            const gitInfo = {
+              "login": login,
+              "avatar_url": avatar_url,
+              "repos_url": repos_url,
+              "email": email
+            }; 
+            return response.avatar_url
+            
+          }).catch(err =>{
+            
+            console.log(err)
+            return null
+          });
+    }catch{(err)=>{
+      console.log(err)
+      return null
+    }}
+
+  }
+  
+  
+  
+  function writeToFile (data, filename) {
+    // console.log(data.projectName)
+    const {username, email, projectName} = data
+   const avatarUrl = getUsername(username);  
+   console.log(avatarUrl)
+    
+    const header = `# ${projectName} 
+    
+        ![https://img.shields.io/badge/Node.js-v12.16.2-brightgreen] ![https://img.shields.io/badge/Electron-v.6.14.4-blue]
+        ${projectDescription}
+        
+        `;
+      fs.writeFile(filename + ".html", header + data, function (err) {
+        if (err) {
+          return console.log(err);
+        }
+    } )
 }
 
 function init() {
