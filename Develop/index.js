b041be33dfd455b2ddebd7d989eb06386608402f
dean <dean@SSCCBISHOP.LOCAL>
2020-04-29 22:40:40 -0400

    generate readme.md

--- Develop/index.js
+++ Develop/index.js
@@ -6,105 +6,173 @@
 
 */
 const axios = require('axios');
-//const dotenv = require('dotenv');
+require('dotenv').config()
+
 //const electron = require('electron');
 //const electronHtmlTo = require('electron-html-to');
 const inquirer = require('inquirer');
 const fs = require('fs');
 //const open = require('open');
 console.log(process.env.SECRET_MESSAGE);
-
+const TOKEN = process.env.TOKEN;
 
 const questions = [
+  // {
+  //   type: "input",
+  //   message: "What is your Public GitHub Email?",
+  //   name: "email"
+  // },
   {
     type: "input",
-    message: "What is your GitHub username?",
-    name: "username"
+    message: "What is your Project's title?",
+    name: "projectTitle"
   },
   {
     type: "input",
-    message: "What is your Public GitHub Email?",
-    name: "email"
+    message: "Describe your Project's purpose?",
+    name: "description"
   },
   {
     type: "input",
-    message: "What is your GitHub Project Name?",
-    name: "projectName"
-  }
+    message: "Do you need a Table of Contents?",
+    name: "tableOfContents"
+  },
+  {
+    type: "input",
+    message: "How do we Install your project?",
+    name: "install"
+  },
+  {
+    type: "input",
+    message: "What will we use this Project for?",
+    name: "usage"
+  },
+  {
+    type: "input",
+    message: "What License do you want to apply to this Project?",
+    name: "license"
+  },
+  {
+    type: "input",
+    message: "Do you have Contrubutors?",
+    name: "contributors"
+  },
+  {
+    type: "input",
+    message: "What License do you want to apply to this Project?",
+    name: "tests"
+  },
+
 ];
 
-inquirer
-  .prompt(questions)
-  .then(answers => {
-    // Use user feedback for... whatever!!
-    console.log("Hello world!")
-    
-    // console.log(answers)
-    // console.log(answers.projectName)
-    // const {username, email, projectName} = answers
-    // console.log(username, email, projectName)
-    writeToFile(answers, "ReadME.md")
-  })
-  .catch(error => {
-    if (error.isTtyError) {
-      console.log("Inquierer: unable to save to file: ",error)
-      // Prompt couldn't be rendered in the current environment
-    } else {
-      // Something else when wrong
-    }
-  });
-
-  const getUsername = async (username) => {
-
-    try{
-      console.log(username);
-      const gitUrl = `https://api.github.com/users/${username}`
-      await axios.get(gitUrl)
-          .then(response =>{
-            const {login, avatar_url, repos_url, email} = response.data;
-            const gitInfo = {
-              "login": login,
-              "avatar_url": avatar_url,
-              "repos_url": repos_url,
-              "email": email
-            }; 
-            return response.avatar_url
-            
-          }).catch(err =>{
-            
-            console.log(err)
-            return null
-          });
-    }catch{(err)=>{
-      console.log(err)
-      return null
-    }}
-
+const getUsername = async (username) => {
+  const gitUrl = `https://api.github.com/users/${username}`; //${TOKEN}
+  try{
+    const response = await axios({
+      method: "get",
+      url: gitUrl,
+      headers: {
+      authorization: `token ${TOKEN}`  
+    }})
+    console.log(response.data)
+    const {login, avatar_url, repos_url, email} = response.data;
+      const gitInfo = {
+        "login": login,
+        "avatar_url": avatar_url,
+        "repos_url": repos_url,
+        "email": email
+      }; 
+      return gitInfo
+  }catch(err){
+    console.log(err)
+    return null
   }
+};
+
+const getUserInput = async () => {
+  try {
   
+    const input = await inquirer.prompt({
+      type: "input",
+      message: "Please enter your GitHub username: ",
+      name: "username"
+    });
+    // const currentUser = await getUsername(input.username)
+    //console.log("In getUserInput: ",currentUser.user)
+    writeToFile(input, "readME.md")
+  } catch (error){
+    console.log("In getUserInput catch error: ",error);
+  }
+};
+
+const getProjectInfo = async () => {
+  try {
+    const input = await inquirer.prompt(questions);
+  writeToFile(input, "readME.md")
+  } catch (error){
+  console.log("In getuserProjectInfo catch error: ", error);
+  }
+}
   
+async function writeToFile (data, filename) {
+  // console.log(data.projectName)
+  const {username, email, projectName, projectUrl} = data
+  const gitInfo = await getUsername(username); 
+  console.log("gitinfo",gitInfo)
+
+  const avatar_url = !gitInfo ? "placeHolder" : gitInfo.avatar_url
+  console.log("avatar url",avatar_url)
+  const gitProject = await getProjectInfo(projectInfo);
+  //const projectTitle = gitProject.projectTitle;
+
+
+
+  const header = `# ${projectName}                       [!${avatar_url}] 
   
-  function writeToFile (data, filename) {
-    // console.log(data.projectName)
-    const {username, email, projectName} = data
-   const avatarUrl = getUsername(username);  
-   console.log(avatarUrl)
-    
-    const header = `# ${projectName} 
-    
-        ![https://img.shields.io/badge/Node.js-v12.16.2-brightgreen] ![https://img.shields.io/badge/Electron-v.6.14.4-blue]
-        ${projectDescription}
-        
-        `;
-      fs.writeFile(filename + ".html", header + data, function (err) {
-        if (err) {
-          return console.log(err);
-        }
-    } )
+      ${projectDescription}
+      [![GitHub issues](https://img.shields.io/github/issues/${username}/${projectName}?style=plastic)]({$projectUrl}/issues)
+      [![GitHub forks](https://img.shields.io/github/forks/${username}/${projectName}?style=plastic)]({$projectUrl}/network)
+      
+      ##${descrition}
+
+      ## Table of Contents
+      
+      * [Installation](#installation)
+      * [Usage](#usage)
+      * [Credits](#credits)
+      * [License](#license)
+      
+      ## Installation
+      ${install}
+
+      ## Usage
+      ```
+      ${usage}
+      ```
+
+      ## Credits
+
+      ${collaborators}
+
+      ## License
+
+      ${license}
+
+      ## Tests
+      ```
+      ${test}
+      ```
+
+      `;
+    fs.writeFile(filename, header + data, function (err) {
+      if (err) {
+        return console.log(err);
+      }
+  } )
 }
 
 function init() {
-
+  getUserInput();
 }
 
 init();
